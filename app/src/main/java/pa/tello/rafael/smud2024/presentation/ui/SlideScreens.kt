87cfedd340ac3cc8f0d05dadc1d9e9040eac9284
Rafael Tello <rafael.tello@sparebank1.no>
2024-01-16 15:50:25 +0100

    In about me slide: picture is shown in grayscale and clipped in a circle

--- app/src/main/java/pa/tello/rafael/smud2024/presentation/ui/SlideScreens.kt
+++ app/src/main/java/pa/tello/rafael/smud2024/presentation/ui/SlideScreens.kt
@@ -2,6 +2,7 @@ package pa.tello.rafael.smud2024.presentation.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,12 +11,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,8 +28,9 @@ import pa.tello.rafael.smud2024.R
 import pa.tello.rafael.smud2024.data.SlideContent
 import pa.tello.rafael.smud2024.presentation.ui.theme.Smud2024Theme
 import pa.tello.rafael.smud2024.presentation.ui.theme.darkPink
+import pa.tello.rafael.smud2024.presentation.ui.theme.lightBlue
+import pa.tello.rafael.smud2024.presentation.ui.theme.lightPink
 import pa.tello.rafael.smud2024.presentation.ui.theme.typography
-import pa.tello.rafael.smud2024.presentation.ui.theme.veryLightGray
 
 
 @Composable
@@ -61,7 +67,7 @@ fun ContentSlide(
 fun AboutMeSlide(
     slideContent: SlideContent
 ) {
-    val backgroundColor = veryLightGray
+    val backgroundColor = lightBlue
     Column(
         Modifier
             .fillMaxSize()
@@ -89,7 +95,11 @@ fun AboutMeSlide(
             Image(
                 painter = painterResource(id = slideContent.image),
                 contentDescription = slideContent.imageDescription,
-                modifier = Modifier.size(400.dp)
+                colorFilter = ColorFilter.colorMatrix(ColorMatrix().apply { setToSaturation(0f) }),
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, lightPink, CircleShape)
             )
         }
         Row(
